1b05b88937fee4748ec793305d42d98bac4ca0af
lincenying <lincenying@qq.com>
2016-11-11 14:14:55 +0800

    *

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,7 +8,7 @@ var article = require('../api/article'),
     user = require('../api/user')
 
 router.all('*',function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', 'http://vue2.mmxiaowu.com')
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Authorization")
     res.header("Access-Control-Allow-Credentials", true)
     if (req.method === 'OPTIONS') {
