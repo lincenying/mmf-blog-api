3a35a0aecab740714db836dda05a3748ece41436
lincenying <lincenying@qq.com>
2016-11-21 01:23:00 +0800

    删除多余的multipartMiddleware中间件

--- server/routes/index.js
+++ server/routes/index.js
@@ -22,11 +22,11 @@ router.post('/admin', (req, res) => {
 })
 // API
 // 管理时, 获取文章列表
-router.get('/api/admin/topics', multipartMiddleware, isLogin, (req, res) => {
+router.get('/api/admin/topics', isLogin, (req, res) => {
     article.getAdminTopics(req, res)
 })
 // 管理时, 获取单篇文章
-router.get('/api/admin/article', multipartMiddleware, isLogin, (req, res) => {
+router.get('/api/admin/article', isLogin, (req, res) => {
     article.getAdminArticle(req, res)
 })
 // 管理时, 发布文章
@@ -34,11 +34,11 @@ router.post('/api/admin/article/post', multipartMiddleware, isLogin, (req, res) 
     article.postArticle(req, res)
 })
 // 管理时, 删除文章
-router.get('/api/admin/article/delete', multipartMiddleware, isLogin, (req, res) => {
+router.get('/api/admin/article/delete', isLogin, (req, res) => {
     article.deleteArticle(req, res)
 })
 // 管理时, 恢复文章
-router.get('/api/admin/article/recover', multipartMiddleware, isLogin, (req, res) => {
+router.get('/api/admin/article/recover', isLogin, (req, res) => {
     article.recoverArticle(req, res)
 })
 // 管理时, 编辑文章
@@ -46,11 +46,11 @@ router.post('/api/admin/article/modify', multipartMiddleware, isLogin, (req, res
     article.modifyArticle(req, res)
 })
 // 前台浏览时, 获取文章列表
-router.get('/api/frontend/topics', multipartMiddleware, (req, res) => {
+router.get('/api/frontend/topics', (req, res) => {
     article.getTopics(req, res)
 })
 // 前台浏览时, 获取单篇文章
-router.get('/api/frontend/article', multipartMiddleware, (req, res) => {
+router.get('/api/frontend/article', (req, res) => {
     article.geArticle(req, res)
 })
 // 发布评论
@@ -58,7 +58,7 @@ router.post('/api/frontend/comment/post', multipartMiddleware, (req, res) => {
     comment.postComment(req, res)
 })
 // 读取评论列表
-router.get('/api/frontend/comment/list', multipartMiddleware, (req, res) => {
+router.get('/api/frontend/comment/list', (req, res) => {
     comment.getComment(req, res)
 })
 // 登录
